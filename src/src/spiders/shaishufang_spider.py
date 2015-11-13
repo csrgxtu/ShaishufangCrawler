f6583164c2ab222d5aad79975758d55a26b8727b
archer <1246506786@qq.com>
2015-11-13 03:57:53 -0500

    I think it starting working

--- src/src/spiders/shaishufang_spider.py
+++ src/src/spiders/shaishufang_spider.py
@@ -4,11 +4,13 @@ class ShaishufangSpider(scrapy.Spider):
     name = "Shaishufang"
     allowed_domains = ["shaishufang.com"]
     start_urls = [
-        "http://shaishufang.com/index.php/site/detail/uid/74557/ubid/10490038/status//category/D/friend/false",
+        'http://shaishufang.com/index.php/site/main/uid/1/status//category//friend/false',
+        'http://shaishufang.com/index.php/site/main/uid/74557/status//category//friend/false',
     ]
 
     def start_requests(self):
-        return [scrapy.Request(self.start_urls[0], cookies={'shaishufang': 'Mjc5MTYwfGZmY2VmYzIyYmMxZjhlZThjNzgzYjFlOGIxOWUwODg2'})]
+        for i in range(len(self.start_urls)):
+            yield scrapy.Request(self.start_urls[i], cookies={'shaishufang': 'Mjc5MTYwfGZmY2VmYzIyYmMxZjhlZThjNzgzYjFlOGIxOWUwODg2'})
 
     def parse(self, response):
         filename = '74557.html'
