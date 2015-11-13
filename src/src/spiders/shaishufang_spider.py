099b6e0b2276a5b2d4b527936952648f165df832
archer <1246506786@qq.com>
2015-11-13 03:49:20 -0500

    overide start_request

--- src/src/spiders/shaishufang_spider.py
+++ src/src/spiders/shaishufang_spider.py
@@ -7,6 +7,9 @@ class ShaishufangSpider(scrapy.Spider):
         "http://shaishufang.com/index.php/site/detail/uid/74557/ubid/10490038/status//category/D/friend/false",
     ]
 
+    def start_requests(self):
+        return [scrapy.Request(self.start_urls[0], cookies={'shaishufang': 'Mjc5MTYwfGZmY2VmYzIyYmMxZjhlZThjNzgzYjFlOGIxOWUwODg2'})]
+
     def parse(self, response):
         filename = '74557.html'
         with open(filename, 'wb') as f:
