8f9b79df1318630eb25fbc57fa07cfb8fbb6a6f6
archer <1246506786@qq.com>
2015-11-14 03:57:30 -0500

    proxy worked, but random proxy isn't

--- src/src/settings.py
+++ src/src/settings.py
@@ -94,7 +94,15 @@ MONGODB_DB = "shaishufang"
 MONGODB_USERS_COLLECTION = "Users"
 MONGODB_BOOKS_COLLECTION = "Books"
 
+
+# Retry many times since proxies often fail
+RETRY_TIMES = 10
+# Retry on most error codes since proxies fail for different reasons
+RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
 # HTTP Proxy enabled
 DOWNLOADER_MIDDLEWARES = {
-'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 1,
+    'scrapy.contrib.downloadermiddleware.retry.RetryMiddleware': 90,
+    # Fix path to this module
+    'yourspider.randomproxy.RandomProxy': 100,
+    'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 110,
 }
