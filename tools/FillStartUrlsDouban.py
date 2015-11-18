8463a1886a37bfc7952fd4388af816c11ceaac47
archer <1246506786@qq.com>
2015-11-18 03:06:35 -0500

    douban initialization ready

--- tools/FillStartUrlsDouban.py
+++ tools/FillStartUrlsDouban.py
@@ -17,17 +17,30 @@ headers = {
 # first, shaishufang
 # http://shaishufang.com/index.php/site/main/uid/2/status//category//friend/false
 # API = 'http://192.168.100.3:5000/unvisitedurls'
-# API = 'http://127.0.0.1:5000/unvisitedurls'
+API = 'http://127.0.0.1:5000/unvisitedurls'
 urlPrefix = 'http://book.douban.com/isbn/'
-for uid in range(1, 2):
-    url = urlPrefix + str(uid) + urlPostfix
-    params = {
-        "urls": [
-            {
-                "url": url,
-                "spider": 'Shaishufang'
-            }
-        ]
-    }
-    response = unirest.put(API, headers=headers, params=json.dumps(params))
-    print response.body
+with open('/home/archer/Documents/BeautifulReading/src/AmazonScraber/isbns.data', 'r') as MH:
+    for line in MH:
+        isbn = line.replace("\n", "")
+        url = urlPrefix + isbn
+        params = {
+            "urls": [
+                {"url": url, "spider": "douban"}
+            ]
+        }
+        res = unirest.put(API, headers=headers, params=json.dumps(params))
+        print res.body
+    # print MH.readline()
+
+# for uid in range(1, 2):
+#     url = urlPrefix + str(uid) + urlPostfix
+#     params = {
+#         "urls": [
+#             {
+#                 "url": url,
+#                 "spider": 'Shaishufang'
+#             }
+#         ]
+#     }
+#     response = unirest.put(API, headers=headers, params=json.dumps(params))
+#     print response.body
